60f5bd809b910badd5eaef26d77c84fd177d493c
Daniel Cardozo <daniel.cardozo0621@gmail.com>
2016-12-09 01:13:30 -0500

    Joineds CRUD finished. Tests pending

--- app/joinedsRoutes.js
+++ app/joinedsRoutes.js
@@ -15,48 +15,49 @@ module.exports = function(app, passport) {
   // Add new register
   app.post('/new-joined',function(req,res,next){
 
-	  var joined = new Joined();
-		joined.fullname 								= req.body.fullname;
-		joined.identityCard 						= req.body.identityCard;
-		joined.birthdate 								= req.body.birthdate;
-		joined.address 									= req.body.address;
-		joined.email 										= req.body.email;
-		joined.rh 											= req.body.rh;
-		joined.annexedDocument 					= req.body.annexedDocument;
-		joined.gender 									= req.body.gender;
-		joined.municipality 						= req.body.municipality;
-		joined.cellPhoneNumber 					= req.body.cellPhoneNumber;
-		joined.educationalInstitution 	= req.body.educationalInstitution;
-		joined.areaPerformance 					= req.body.areaPerformance;
-		joined.university 							= req.body.university;
-		joined.normalista 							= req.body.normalista;
-		joined.specialist 							= req.body.specialist;
-		joined.doctorate 								= req.body.doctorate;
-		joined.typeOfAppointment 				= req.body.typeOfAppointment;
-		joined.state 										= req.body.state;
-		joined.municipalityWorking 			= req.body.municipalityWorking;
-		joined.degree 									= req.body.degree;
-		joined.professional 						= req.body.professional;
-		joined.MasterDegree 						= req.body.MasterDegree;
-		joined.promotionLadder 					= req.body.promotionLadder;
-		joined.Teaching 								= req.body.Teaching;
-		joined.level 										= req.body.level;
-		joined.municipalitySindical 		= req.body.municipalitySindical;
-		joined.affiliationPaymentType 	= req.body.affiliationPaymentType;
-		joined.integraCommission 				= req.body.integraCommission;
-		joined.Zone 										= req.body.Zone;
-		joined.integraCommittee 				= req.body.integraCommittee;
-		joined.delegateGeneralAssembly	= req.body.delegateGeneralAssembly;
-		joined.employeeClass						= req.body.employeeClass;
+	  	var objJoined = new Joined();
+
+		objJoined.identityCard 						= req.body.identityCard;
+		objJoined.fullname 								= req.body.fullname;
+		objJoined.birthdate 								= req.body.birthdate;
+		objJoined.address 									= req.body.address;
+		objJoined.email 										= req.body.email;
+		objJoined.rh 											= req.body.rh;
+		objJoined.gender 									= req.body.gender;
+		objJoined.municipality 						= req.body.municipality;
+		objJoined.cellPhoneNumber 					= req.body.cellPhoneNumber;
+		objJoined.educationalInstitution 	= req.body.educationalInstitution;
+		objJoined.areaPerformance 					= req.body.areaPerformance;
+		objJoined.university 							= req.body.university;
+		objJoined.normalista 							= req.body.normalista;
+		objJoined.specialist 							= req.body.specialist;
+		objJoined.doctorate 								= req.body.doctorate;
+		objJoined.typeOfAppointment 				= req.body.typeOfAppointment;
+		objJoined.state 										= req.body.state;
+		objJoined.municipalityWorking 			= req.body.municipalityWorking;
+		objJoined.degree 									= req.body.degree;
+		objJoined.professional 						= req.body.professional;
+		objJoined.MasterDegree 						= req.body.MasterDegree;
+		objJoined.promotionLadder 					= req.body.promotionLadder;
+		objJoined.Teaching 								= req.body.Teaching;
+		objJoined.level 										= req.body.level;
+		objJoined.municipalitySindical 		= req.body.municipalitySindical;
+		objJoined.affiliationPaymentType 	= req.body.affiliationPaymentType;
+		objJoined.integraCommission 				= req.body.integraCommission;
+		objJoined.Zone 										= req.body.Zone;
+		objJoined.integraCommittee 				= req.body.integraCommittee;
+		objJoined.delegateGeneralAssembly	= req.body.delegateGeneralAssembly;
+		objJoined.employeeClass						= req.body.employeeClass;
     
-    joined.save(function (err) {
-      if (err){
-        console.log('err: '+err);
-      } else {
-        console.log('Objeto almacenado');
-        res.send('/joineds');
-      }
-    });
+	    objJoined.save(function (err) {
+	      if (err){
+	        console.log('Error creando objeto: '+err);
+	        res.redirect('/joineds');
+	      } else {
+	        console.log('Objeto almacenado');
+	        res.redirect('/joineds');
+	      }
+	    });
   });
 
   // Read object data
@@ -77,49 +78,50 @@ module.exports = function(app, passport) {
     var id = req.param("id");
 
     Joined.findById(id, function(err, objJoined){
-      if (err) ;
+  		if (err) ;
 
-	    objJoined.fullname 								= req.body.fullname;
-			objJoined.identityCard 						= req.body.identityCard;
-			objJoined.birthdate 							= req.body.birthdate;
-			objJoined.address 								= req.body.address;
-			objJoined.email 									= req.body.email;
-			objJoined.rh 											= req.body.rh;
-			objJoined.annexedDocument 				= req.body.annexedDocument;
-			objJoined.gender 									= req.body.gender;
-			objJoined.municipality 						= req.body.municipality;
-			objJoined.cellPhoneNumber 				= req.body.cellPhoneNumber;
-			objJoined.educationalInstitution 	= req.body.educationalInstitution;
-			objJoined.areaPerformance 				= req.body.areaPerformance;
-			objJoined.university 							= req.body.university;
-			objJoined.normalista 							= req.body.normalista;
-			objJoined.specialist 							= req.body.specialist;
-			objJoined.doctorate 							= req.body.doctorate;
-			objJoined.typeOfAppointment 			= req.body.typeOfAppointment;
-			objJoined.state 									= req.body.state;
-			objJoined.municipalityWorking 		= req.body.municipalityWorking;
-			objJoined.degree 									= req.body.degree;
-			objJoined.professional 						= req.body.professional;
-			objJoined.MasterDegree 						= req.body.MasterDegree;
-			objJoined.promotionLadder 				= req.body.promotionLadder;
-			objJoined.Teaching 								= req.body.Teaching;
-			objJoined.level 									= req.body.level;
-			objJoined.municipalitySindical 		= req.body.municipalitySindical;
-			objJoined.affiliationPaymentType 	= req.body.affiliationPaymentType;
-			objJoined.integraCommission 			= req.body.integraCommission;
-			objJoined.Zone 										= req.body.Zone;
-			objJoined.integraCommittee 				= req.body.integraCommittee;
-			objJoined.delegateGeneralAssembly	= req.body.delegateGeneralAssembly;
-			objJoined.employeeClass						= req.body.employeeClass;
+    	objJoined.fullname 								= req.body.fullname;
+		objJoined.identityCard 						= req.body.identityCard;
+		objJoined.birthdate 							= req.body.birthdate;
+		objJoined.address 								= req.body.address;
+		objJoined.email 									= req.body.email;
+		objJoined.rh 											= req.body.rh;
+		objJoined.gender 									= req.body.gender;
+		objJoined.municipality 						= req.body.municipality;
+		objJoined.cellPhoneNumber 				= req.body.cellPhoneNumber;
+		objJoined.educationalInstitution 	= req.body.educationalInstitution;
+		objJoined.areaPerformance 				= req.body.areaPerformance;
+		objJoined.university 							= req.body.university;
+		objJoined.normalista 							= req.body.normalista;
+		objJoined.specialist 							= req.body.specialist;
+		objJoined.doctorate 							= req.body.doctorate;
+		objJoined.typeOfAppointment 			= req.body.typeOfAppointment;
+		objJoined.state 									= req.body.state;
+		objJoined.municipalityWorking 		= req.body.municipalityWorking;
+		objJoined.degree 									= req.body.degree;
+		objJoined.professional 						= req.body.professional;
+		objJoined.MasterDegree 						= req.body.MasterDegree;
+		objJoined.promotionLadder 				= req.body.promotionLadder;
+		objJoined.Teaching 								= req.body.Teaching;
+		objJoined.level 									= req.body.level;
+		objJoined.municipalitySindical 		= req.body.municipalitySindical;
+		objJoined.affiliationPaymentType 	= req.body.affiliationPaymentType;
+		objJoined.integraCommission 			= req.body.integraCommission;
+		objJoined.Zone 										= req.body.Zone;
+		objJoined.integraCommittee 				= req.body.integraCommittee;
+		objJoined.delegateGeneralAssembly	= req.body.delegateGeneralAssembly;
+		objJoined.employeeClass						= req.body.employeeClass;
 		
 
-      objEvent.save({_id:id}, function(err){
-        if (err) {
-          res.redirect("/joineds");
-        } else{
-          res.redirect("/joineds");
-        }
-      });
+      	objJoined.save({_id:id}, function(err){
+	        if (err) {
+	        	console.log("Error modificando objeto")
+	          	res.redirect("/joineds");
+	        } else{
+	        	console.log("Objeto modificado")
+	          	res.redirect("/joineds");
+	        }
+      	});
     });
   });
 };
