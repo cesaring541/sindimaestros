165562cf9cd1dabf9ef0cadf3f723b2a8aed7a09
Daniel Cardozo <daniel.cardozo0621@gmail.com>
2016-12-09 15:46:47 -0500

    Mlab Database changed to DanCard

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://sindimaestros:admin123@ds127428.mlab.com:27428/sindimastros'
-    //'url' : 'mongodb://wennova:admin123@ds155087.mlab.com:55087/sindimaestros'
+    //'url' : 'mongodb://sindimaestros:admin123@ds127428.mlab.com:27428/sindimastros'
+    'url' : 'mongodb://wennova:admin123@ds155087.mlab.com:55087/sindimaestros'
 };
