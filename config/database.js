6ca468fe271a8d67a9dd30d0033353fc008a12a8
We-nnova <cesaring541@gmail.com>
2017-02-06 14:08:54 +0000

    Database linked to localhost

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 module.exports = {
 
 	//'url' : 'mongodb://sindimaestros:admin123@ds127428.mlab.com:27428/sindimastros'
-	'url' : 'mongodb://wennova:admin123@ds155087.mlab.com:55087/sindimaestros'
+	//'url' : 'mongodb://wennova:admin123@ds155087.mlab.com:55087/sindimaestros'
 	//'url' : 'mongodb://admin:Admin.123@localhost:27017/sindi'
+	'url' : 'mongodb://localhost/sindimaestros'
 
 };
