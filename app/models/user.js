d178238047879baf4cb986816488f1b347b0bfbc
Daniel Cardozo <daniel.cardozo0621@gmail.com>
2017-02-08 10:36:52 -0500

    Zone attribute added to Users model

--- app/models/user.js
+++ app/models/user.js
@@ -5,23 +5,23 @@ var required = require('required');
 var bcrypt   = require('bcrypt-nodejs');
 
 var userSchema = mongoose.Schema({
-    joined		: { type: String, required: true,  enum: ['Si', 'No'] },
-    fullname  : { type: String, required: true },
-    email			: { type: String },
-    password 	: { type: String },
-    role 			: { type: String, required: true,  enum: ['Administrador', 'Presidente', 'Presidente Municipal', 'Presidente Zonal', 'Fiscal', 'Tesorero'] },
-    zone			: { type: String },
-    municipality : { type: String }
+  joined		: { type: String, required: true,  enum: ['Si', 'No'] },
+  fullname  : { type: String, required: true },
+  email			: { type: String },
+  password 	: { type: String },
+  role 			: { type: String, required: true,  enum: ['Administrador', 'Presidente', 'Presidente Municipal', 'Presidente Zonal', 'Fiscal', 'Tesorero'] },
+  zone			: { type: String },
+  municipality : { type: String }
 });
 
 // generating a hash
 userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 // checking if password is valid
 userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
+  return bcrypt.compareSync(password, this.password);
 };
 
 userSchema.plugin(mongoosePaginate);
--- public/js/mainUsers.js
+++ public/js/mainUsers.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
+
+	$.ajax({ 
+		type: 'GET', 
+		url: 'json/zones.json', 
+		dataType: 'json',
+		success: function (data) {
+			var array = data[1].ZONA;
+			for (var i = 0; i < array.length; i++) {
+				$("#zone_list, #mod_zone_list").append("<option value="+ array[i] +">" + array[i] + "</option>");
+			};
+			$("#zone_list, #mod_zone_list").selectpicker("refresh");
+		},
+		error:function(msg) {
+			console.log(msg+" Listado de objetos fallido");
+		}
+	});
+
 	$.ajax({
 		type: 'GET',
 		url: 'json/municipalities.json',
@@ -6,19 +23,24 @@ $(document).ready(function(){
 		success: function (data) {
 			var array = data[1].municipality;
 			for (var i = 0; i < array.length; i++) {
-				$("#municipality_list, #mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+				$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 			};
-			$("#municipality_list, #mod_municipality_list").selectpicker("refresh");
+			$("#mod_municipality_list").selectpicker("refresh");
 		},
 		error:function(msg) {
 			console.log(msg+" Listado de objetos fallido");
 		}
 	});
 
+
 	// ====================================================================================================
 	// DESPLIEGUE DE MUNICIPIOS DE ACUERDO A ZONA SELECCIONADA ============================================
 
 	$('#zone_list').on('change', function() { 
+		refreshMunicipalities();
+	});
+
+	function refreshMunicipalities(){
 		$("#municipality_list").empty();
 		
 		if ($('#zone_list').val()=="CENTRO") {
@@ -31,6 +53,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -46,6 +69,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -61,6 +85,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -76,6 +101,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -91,6 +117,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -106,6 +133,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -121,6 +149,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -136,6 +165,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -151,6 +181,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -166,6 +197,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -181,6 +213,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -196,6 +229,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -211,6 +245,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -226,6 +261,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -241,6 +277,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -256,6 +293,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -271,6 +309,7 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
@@ -286,13 +325,313 @@ $(document).ready(function(){
 					for (var i = 0; i < array.length; i++) {
 						$("#municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
 					};
+					$("#municipality_list").selectpicker("refresh");
 				},
 				error:function(msg) {
 					console.log(msg+" Listado de objetos fallido");
 				}
 			});
 		}
+	}
+
+
+	$('#mod_zone_list').on('change', function() { 
+		refreshModMunicipalities();
 	});
+
+	function refreshModMunicipalities(){
+		$("#mod_municipality_list").empty();
+		
+		if ($('#mod_zone_list').val()=="CENTRO") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/centro.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].CENTRO;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="GUATEQUE") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/guateque.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].GUATEQUE;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="RICAURTE-BAJO") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/ricaurte-bajo.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].RICAURTE-BAJO;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="RICAURTE-ALTO") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/ricaurte-alto.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].RICAURTE-ALTO;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="TUNDAMA") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/tundama.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].TUNDAMA;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="DUITAMA") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/duitama.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].DUITAMA;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="VALDERRAMA") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/valderrama.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].VALDERRAMA;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="GARAGOA") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/garagoa.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].GARAGOA;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="GUTIERREZ") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/gutierrez.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].GUTIERREZ;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="NORTE") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/norte.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].NORTE;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="SUGAMUXI") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/sugamuxi.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].SUGAMUXI;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="SOGAMOSO") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/sogamoso.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].SOGAMOSO;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="LENGUPA") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/lengupa.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].LENGUPA;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="OCCIDENTE") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/occidente.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].OCCIDENTE;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="MARQUEZ") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/marquez.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].MARQUEZ;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="PUERTO-BOYACÁ") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/puerto.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].PUERTO-BOYACÁ;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="TUNJA") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/tunja.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].TUNJA;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		} else if ($('#mod_zone_list').val()=="CHIQUINQUIRA") {
+			$.ajax({ 
+				type: 'GET', 
+				url: 'json/chiquinquira.json', 
+				dataType: 'json',
+				success: function (data) {
+					var array = data[1].CHIQUINQUIRA;
+					for (var i = 0; i < array.length; i++) {
+						$("#mod_municipality_list" ).append("<option value="+ array[i] +">" + array[i] + "</option>");
+					};
+					$("#mod_municipality_list").selectpicker("refresh");
+				},
+				error:function(msg) {
+					console.log(msg+" Listado de objetos fallido");
+				}
+			});
+		}
+	}
 
 	// ====================================================================================================
 	
